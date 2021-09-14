bfa9b71a8d6ea215a768d332a69615240944fef8
siily <arnokosk15@gmail.com>
2021-09-14 09:47:23 +0300

    new js lines

--- dice.js
+++ dice.js
@@ -1,41 +1,20 @@
-var turns = 4;
+// var turns = 3;
 
 
-$('#roll').click(function () {
-    var cubes = $('.diceid:not(.held)');
-    cubes.each(function () {
-        roll($(this));
-    });
-    turns--;
+// $('#roll').click(function () {
+//     var cubes = $('.diceid:not(.held)');
+//     cubes.each(function () {
+//         roll($(this));
+//     });
+//     turns--;
 
-    $('#turns').text(turns);
+//     $('#turns').text(turns);
 
-    if (turns === 0) {
-        $(this).unbind('click');
-        $(this).attr('disabled', 'disabled');
-    }
-});
-turns--;
-
-$('#turns').text(turns);
-
-if (turns === 0) {
-    $(this).unbind('click');
-    $(this).attr('disabled', 'disabled');
-};
-function roll() {
-    const rng1 = Math.floor(Math.random() * 6) + 1
-    document.querySelector(".dice1").setAttribute("src", rng1 + ".png");
-    const rng2 = Math.floor(Math.random() * 6) + 1
-    document.querySelector(".dice2").setAttribute("src", rng2 + ".png");
-    const rng3 = Math.floor(Math.random() * 6) + 1
-    document.querySelector(".dice3").setAttribute("src", rng3 + ".png");
-    const rng4 = Math.floor(Math.random() * 6) + 1
-    document.querySelector(".dice4").setAttribute("src", rng4 + ".png");
-    const rng5 = Math.floor(Math.random() * 6) + 1
-    document.querySelector(".dice5").setAttribute("src", rng5 + ".png");
-}
-
+//    if (turns === 0) {
+//        $(this).unbind('click');
+//        $(this).attr('disabled', 'disabled');
+//    }
+//});
 let diceData = [
     {
         value: undefined,
@@ -63,13 +42,16 @@ let diceData = [
 
 const diceDivs = document.querySelectorAll('.dice');
 
-diceDivs.forEach( (dice) => {
-    if (dice.dataset.status == 'active') {
-        dice.dataset.status == 'locked';
-        diceData[dice.dataset.index].status ='active'
-    } else {
-        dice.dataset.status = 'active';
-        diceData[dice.dataset.index] = 'active';
-    }
+diceDivs.forEach((dice) => {
+    dice.addEventListener('click', () => {
+        if (dice.dataset.status == 'active') {
+            dice.dataset.status == 'locked';
+            diceData[dice.dataset.index].status = 'active'
+        } else {
+            dice.dataset.status = 'active';
+            diceData[dice.dataset.index] = 'active';
+
+        }
+    });
     console.log(diceData)
 });
